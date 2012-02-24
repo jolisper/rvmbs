06e4ef7700ac6f87905cd3c07a20e926f7b549a4
jolisper <jolisper@gmail.com>
2012-02-24 01:51:44 -0300

    Modifies daemon command for compatibility RUBY_VERSION < 1.9

--- lib/rvmbs.rb
+++ lib/rvmbs.rb
@@ -78,7 +78,17 @@ module RVMBS
     def self.set_rvmrc_trusted(options, current_dir)
       current_dir = 
       pid = fork do
-        Process.daemon
+        if RUBY_VERSION < "1.9"
+          exit if fork
+          Process.setsid
+          exit if fork
+          Dir.chdir "/"
+          STDIN.reopen "/dev/null"
+          STDOUT.reopen "/dev/null", "a"
+          STDERR.reopen "/dev/null", "a"
+        else
+          Process.daemon
+        end
         exit system "rvm rvmrc trust #{current_dir}/#{options[:directory]}/"
       end
       _, status = Process.waitpid2(pid)
